403d96fa9fa7b2160f160f9b7ce32806ee259842
Slavi Boyanov <slavi.boyanov@gmail.com>
2019-10-01 23:38:48 +0300

    Version without external deps

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const exporter = new Exporter(pkg.name)
 const SEND_BATCH_SIZE = parseInt(process.env.SEND_BATCH_SIZE || "10")
 const DEFAULT_TIMEOUT = parseInt(process.env.DEFAULT_TIMEOUT || "10000")
 // const CONFIRMATIONS = parseInt(process.env.CONFIRMATIONS || "3")
-const NODE_URL = process.env.NODE_URL || 'http://litecoind.default.svc.cluster.local:9332'
+const NODE_URL = process.env.NODE_URL || 'http://localhost:4000'
 const RPC_USERNAME = process.env.RPC_USERNAME || 'rpcuser'
 const RPC_PASSWORD = process.env.RPC_PASSWORD || 'rpcpassword'
 
@@ -29,11 +29,11 @@ const request = rp.defaults({
 })
 
 let lastProcessed = {
-  blockNumber: parseInt(process.env.BLOCK || "1"),
+  blockNumber: parseInt(process.env.BLOCK || "0"),
 }
 
 const sendRequest = (async (method, params) => {
-  metrics.requestsCounter.inc()
+  // metrics.requestsCounter.inc()
 
   const startTime = new Date()
   return request({
@@ -44,7 +44,7 @@ const sendRequest = (async (method, params) => {
       params: params
     }
   }).then(({ result, error }) => {
-    metrics.requestsResponseTime.observe(new Date() - startTime)
+    // metrics.requestsResponseTime.observe(new Date() - startTime)
 
     if(error) {
       return Promise.reject(error)
@@ -67,74 +67,75 @@ const fetchBlockByIndex = async (blockIndex) => {
   return await fetchBlockByHash(blockHash)
 }
 
-async function rewindOrphans() {
-  let blocksToRewind = []
+async function rollbackOrphans() {
+  let blocks = []
   let lastProcessedHash = lastProcessed.blockHash
   let lastExportedBlock = await fetchBlockByHash(lastProcessedHash);
 
   while (lastExportedBlock.confirmations === -1) {
-    blocksToRewind.unshift(lastExportedBlock)
+    blocks.unshift(lastExportedBlock)
     lastProcessedHash = lastExportedBlock.previousblockhash
     lastExportedBlock = await fetchBlockByHash(lastProcessedHash);
   }
-
-  if(blocksToRewind.lenght()) {
+  if(blocks.length) {
 
     // TODO::RED find if we need to decrement this counter
 
-    // const blocks = await Promise.all(blocksToRewind).map(async (block) => {
+    // const blocks = await Promise.all(blocks).map(async (block) => {
     //   metrics.downloadedTransactionsCounter.dec(block.tx.length)
     //   metrics.downloadedBlocksCounter.dec()
 
     //   return block
     // })
 
-    console.log(`Rewinding blocks ${blocksToRewind[0].height}:${blocksToRewind[blocks.length - 1].height}`)
-    await exporter.sendDataWithKey(blocksToRewind, "height")
+    console.info(`Rollback blocks ${blocks[0].height}:${blocks[blocks.length - 1].height}`)
+    // await exporter.sendDataWithKey(blocks, "height")
 
     lastProcessed.blockNumber -= blocks.length
-    lastProcessed.blockHash = lastHash
+    lastProcessed.blockHash = lastProcessedHash
     await saveNewPosition()
   }
 }
 
 
 async function saveNewPosition()  {
-  await exporter.savePosition(lastProcessed)
-  metrics.lastExportedBlock.set(lastProcessed.blockNumber)
+  // await exporter.savePosition(lastProcessed)
+  // metrics.lastExportedBlock.set(lastProcessed.blockNumber)
 }
 
 
 async function work() {
-  await rewindOrphans()
+  await rollbackOrphans()
 
   const blockchainInfo = await sendRequest('getblockchaininfo', [])
   const currentBlock = blockchainInfo.blocks
+  console.info(`current block ${currentBlock}`)
 
-  metrics.currentBlock.set(currentBlock)
+  // metrics.currentBlock.set(currentBlock)
 
   const requests = []
 
-  while (lastProcessed.blockNumber + requests.length < currentBlock) {
+  while (lastProcessed.blockNumber + requests.length <= currentBlock) {
     const blockToDownload = lastProcessed.blockNumber + requests.length
+    console.info(`block to download ${blockToDownload}`)
 
     requests.push(fetchBlockByIndex(blockToDownload))
 
     if (requests.length >= SEND_BATCH_SIZE || blockToDownload == currentBlock) {
       const blocks = await Promise.all(requests).map(async (block) => {
-        metrics.downloadedTransactionsCounter.inc(block.tx.length)
-        metrics.downloadedBlocksCounter.inc()
+        // metrics.downloadedTransactionsCounter.inc(block.tx.length)
+        // metrics.downloadedBlocksCounter.inc()
 
         return block
       })
 
-      console.log(`Flushing blocks ${blocks[0].height}:${blocks[blocks.length - 1].height}`)
-      await exporter.sendDataWithKey(blocks, "height")
+      console.info(`Flushing blocks ${blocks[0].height}:${blocks[blocks.length - 1].height}`)
+      // await exporter.sendDataWithKey(blocks, "height")
 
-      lastProcessedPosition.blockNumber += blocks.length
-      lastProcessedPosition.blockHash = blocks[blocks.length - 1].hash
-      await exporter.savePosition(lastProcessedPosition)
-      metrics.lastExportedBlock.set(lastProcessedPosition.blockNumber)
+      lastProcessed.blockNumber += blocks.length
+      lastProcessed.blockHash = blocks[blocks.length - 1].hash
+      // await exporter.savePosition(lastProcessed)
+      // metrics.lastExportedBlock.set(lastProcessed.blockNumber)
 
       requests.length = 0
     }
@@ -142,7 +143,9 @@ async function work() {
 }
 
 async function initLastProcessedLedger() {
-  const lastPosition = await exporter.getLastPosition()
+  // const lastPosition = await exporter.getLastPosition()
+
+  const lastPosition = false
 
   if (lastPosition) {
     lastProcessed = lastPosition
@@ -156,7 +159,7 @@ async function initLastProcessedLedger() {
   if (lastPosition) {
     console.info(`Resuming export from position ${JSON.stringify(lastProcessed)}`)
   } else {
-    await exporter.savePosition(lastProcessed)
+    // await exporter.savePosition(lastProcessed)
     console.info(`Initialized exporter with initial position ${JSON.stringify(lastProcessed)}`)
   }
 }
@@ -165,13 +168,13 @@ const fetchEvents = () => {
   return work()
     .then(() => {
       // Look for new events every 1 sec
-      setTimeout(fetchEvents, 1000)
+      setTimeout(fetchEvents, 10000)
     })
 }
 
 const init = async () => {
-  metrics.startCollection()
-  await exporter.connect()
+  // metrics.startCollection()
+  // await exporter.connect()
   await initLastProcessedLedger()
   await fetchEvents()
 }
